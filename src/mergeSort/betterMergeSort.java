1fe376c7633e01b6fed2c6fbee446b076378d1a6
Gorkiy Chocolate <beybagame@gmail.com>
2025-09-24 01:39:26 +0500

    MergeSort + insertion sort

---
+++ src/mergeSort/betterMergeSort.java
@@ -0,0 +1,61 @@
+package mergeSort;
+
+public class betterMergeSort {
+    public static void mergeSort(int[] arr) {
+        int[] buffer = new int[arr.length];
+        mergeSort(arr, buffer, 0, arr.length - 1);
+    }
+
+    public static void mergeSort(int[] arr, int[] buffer, int left, int right) {
+        int cutoff = 10;
+        if (right - left + 1 <= cutoff) {
+            insertionSort(arr, left, right);
+            return;
+        }
+
+        if (left >= right) return;
+
+        int mid = (left + right) / 2;
+        mergeSort(arr, buffer, left, mid);
+        mergeSort(arr, buffer, mid + 1, right);
+        merge(arr, buffer, left, mid, right);
+    }
+
+    public static void insertionSort(int[] arr, int left, int right) {
+        for (int i = left + 1; i <= right; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= left && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void merge(int[] arr, int[] buffer, int left, int mid, int right) {
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                buffer[k++] = arr[i++];
+            } else {
+                buffer[k++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) {
+            buffer[k++] = arr[i++];
+        }
+
+        while (j <= right) {
+            buffer[k++] = arr[j++];
+        }
+
+        for (i = left; i <= right; i++) {
+            arr[i] = buffer[i];
+        }
+    }
+}
---
+++ src/mergeSort/mergeSortbasic.java
@@ -0,0 +1,49 @@
+/*package mergeSort;
+
+/*public class mergeSortbasic {
+    public static void mergeSort(int[] arr) {
+        int n = arr.length;
+        if (n == 1) return;
+
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
+
+        for (int i = 0; i < mid; i++)
+            l[i] = arr[i];
+
+        for (int i = mid; i < n; i++)
+            r[i - mid] = arr[i];
+
+        mergeSort(l);
+        mergeSort(r);
+        merge(arr, l, r);
+    }
+
+    public static void merge(int[] arr, int[] l, int[] r) {
+        int left = l.length;
+        int right = r.length;
+        int i = 0;
+        int j = 0;
+        int idx = 0;
+
+
+        while (i < left && j < right) {
+            if (l[i] < r[j]) {
+                arr[idx] = l[i];
+                i++;
+                idx++;
+            } else {
+                arr[idx] = r[j];
+                j++;
+                idx++;
+            }
+        }
+
+        for (int ll = i; ll < left; ll++)
+            arr[idx++] = l[ll];
+        for (int rr = j; rr < right; rr++)
+            arr[idx++] = r[rr];
+
+    }
+}*/
